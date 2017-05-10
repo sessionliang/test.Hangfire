22698b3e20401f2dfb0f619ecb31cf77b36d2816
sessionliang <sessionliang@outlook.com>
2017-05-10 10:09:35 +0800

    first commit

---
+++ Web/App_Start/Startup.Hangfire.cs
@@ -0,0 +1,38 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Hangfire.Redis;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web
+{
+    public partial class Startup
+    {
+        public void ConfigureHangfire(IAppBuilder app)
+        {
+            //Hangfire+SqlServer(免费版)
+            //GlobalConfiguration.Configuration.UseSqlServerStorage("<name or connection string>");
+            //app.UseHangfireDashboard();
+            //app.UseHangfireServer();
+
+
+            //Hangfire_net40+Redis(version:1.1.1)
+            //app.UseHangfire(config =>
+            //{
+            //    config.UseRedisStorage("123123@127.0.0.1", 0, new Hangfire.Redis.RedisStorageOptions());
+            //    config.UseServer();
+            //});
+
+            //Hangfire+Redis.StackExchange(免费版)
+            GlobalConfiguration.Configuration.UseRedisStorage("localhost,password=123123", new Hangfire.Redis.RedisStorageOptions() {
+               
+            });
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+        }
+    }
+}
---
+++ Web/Controllers/TestController.cs
@@ -0,0 +1,109 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public static int Counter = 0;
+
+        // GET: Test
+        public ActionResult Index()
+        {
+            string result = string.Empty;
+
+            BackgroundJob.Enqueue(() => StringJoin(result, "Fire-and-forget" + Counter));
+            Response.Write("<br />");
+            
+            BackgroundJob.Schedule(() => StringJoin(result, "Delayed" + Counter), TimeSpan.FromDays(1));
+            Response.Write("<br />");
+     
+            RecurringJob.AddOrUpdate(() => StringJoin(result, "Daily Job" + Counter), Cron.Daily);
+            Response.Write("<br />");
+
+            return Content(result);
+        }
+
+        public void StringJoin(string str1, string str2)
+        {
+            str1 = str1 + str2;
+            Counter++;
+        }
+
+        // GET: Test/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Test/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Test/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Test/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Test/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Test/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Test/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
